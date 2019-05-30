81aaf0b626495479d630bd814525ee71a8a17653
niklr <r.niklaus@gmx.ch>
2019-05-30 14:56:40 +0200

    Add icons and update font-awesome to 5.8.2

--- src/AMI.Website/ClientApp/src/app/modules/layout/components/topnav/topnav.component.ts
+++ src/AMI.Website/ClientApp/src/app/modules/layout/components/topnav/topnav.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import M from 'materialize-css';
+
 @Component({
-    selector: 'app-topnav',
-    templateUrl: './topnav.component.html',
-    styleUrls: ['./topnav.component.scss']
+  selector: 'app-topnav',
+  templateUrl: './topnav.component.html',
+  styleUrls: ['./topnav.component.scss']
 })
 export class TopnavComponent implements OnInit, AfterViewInit {
 
-    constructor(public router: Router) {
+  constructor(public router: Router) {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-    }
+  }
 
-    ngAfterViewInit(): void {
+  ngAfterViewInit(): void {
+    this.initSidenav();
+  }
 
-    }
+  private initSidenav(): void {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = {};
+    var instances = M.Sidenav.init(elems, options);
+  }
 }
