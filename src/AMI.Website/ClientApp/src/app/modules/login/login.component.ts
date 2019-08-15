c48e6a76d2e30a41fce018c4ea0d3260e723dd0d
Roman Niklaus <r.niklaus@gmx.ch>
2019-08-15 11:29:21 +0200

    Enable click once in login component

--- src/AMI.Website/ClientApp/src/app/modules/login/login.component.ts
+++ src/AMI.Website/ClientApp/src/app/modules/login/login.component.ts
@@ -2,60 +2,69 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { CallbackWrapper } from '../../wrappers/callback.wrapper';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html'
+  selector: 'app-login',
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit, AfterViewInit {
 
-    loginFormGroup = this.fb.group({
-        username: ['', {
-            validators: Validators.required
-        }],
-        password: ['', {
-            validators: Validators.required
-        }]
-    });
+  loginFormGroup = this.fb.group({
+    username: ['', {
+      validators: Validators.required
+    }],
+    password: ['', {
+      validators: Validators.required
+    }]
+  });
 
-    constructor(private router: Router, private fb: FormBuilder, private authService: AuthService) { }
+  constructor(private router: Router, private fb: FormBuilder, private authService: AuthService) { }
 
-    ngOnInit() {
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+  }
+
+  public login = (callbackFn) => {
+    const callbackWrapper = new CallbackWrapper(callbackFn);
+    if (this.loginFormGroup.valid) {
+      const username = this.loginFormGroup.get('username').value;
+      const password = this.loginFormGroup.get('password').value;
+      if (!!username && !!password) {
+        this.authService.login(username, password).then(
+          (s) => {
+            setTimeout(() => {
+              callbackWrapper.invokeCallbackFn();
+              this.router.navigate(['/']);
+            }, 100);
+          },
+          (e) => {
+            callbackWrapper.invokeCallbackFn();
+            this.loginFormGroup.reset();
+          });
+      }
+    } else {
+      setTimeout(() => {
+        callbackWrapper.invokeCallbackFn();
+      }, 100);
     }
+  }
 
-    ngAfterViewInit() {
-    }
-
-    login($event: any): void {
-        $event.preventDefault();
-        if (this.loginFormGroup.valid) {
-            const username = this.loginFormGroup.get('username').value;
-            const password = this.loginFormGroup.get('password').value;
-            if (!!username && !!password) {
-                this.authService.login(username, password).then(
-                    (s) => {
-                        setTimeout(() => {
-                            this.router.navigate(['/']);
-                        }, 100);
-                    },
-                    (e) => {
-                        this.loginFormGroup.reset();
-                    });
-            }
-        }
-    }
-
-    anonymous($event: any): void {
-        $event.preventDefault();
-        this.authService.logout();
-        this.authService.init().then(
-            (s) => {
-                setTimeout(() => {
-                    this.router.navigate(['/']);
-                }, 100);
-            },
-            (e) => {
-                this.loginFormGroup.reset();
-            });
-    }
+  public anonymous = (callbackFn) => {
+    const callbackWrapper = new CallbackWrapper(callbackFn);
+    this.authService.logout();
+    this.authService.init().then(
+      (s) => {
+        setTimeout(() => {
+          callbackWrapper.invokeCallbackFn();
+          this.router.navigate(['/']);
+        }, 100);
+      },
+      (e) => {
+        callbackWrapper.invokeCallbackFn();
+        this.loginFormGroup.reset();
+      });
+  }
 }
--- src/AMI.Website/ClientApp/src/app/modules/login/login.module.ts
+++ src/AMI.Website/ClientApp/src/app/modules/login/login.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { SharedCommonModule } from '../common/common.module';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        LoginRoutingModule
-    ],
-    declarations: [
-        LoginComponent
-    ],
-    providers: [
+  imports: [
+    CommonModule,
+    SharedCommonModule,
+    ReactiveFormsModule,
+    LoginRoutingModule
+  ],
+  declarations: [
+    LoginComponent
+  ],
+  providers: [
 
-    ]
+  ]
 })
-export class LoginModule {}
+export class LoginModule { }
